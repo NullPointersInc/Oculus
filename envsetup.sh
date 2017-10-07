93bab63c9e2344700c074e0e0423c209327c47a2
ujwalp15 <ujwalp09@gmail.com>
2017-10-07 20:54:19 +0530

    oculus: add script to setup environment

---
+++ envsetup.sh
@@ -0,0 +1,9 @@
+echo "This will work only with python3"
+
+sudo apt-get install python3-pip python3-dev
+
+sudo pip3 install tensorflow
+
+sudo apt-get install protobuf-compiler python-pil python-lxml
+sudo pip3 install jupyter
+sudo pip3 install matplotlib
