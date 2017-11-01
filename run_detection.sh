a90e39788d5dfe6c9af0ec3c593bf0ba9b2f5290
ujwalp15 <ujwalp09@gmail.com>
2017-11-01 14:16:47 +0530

    oculus: update run_detection to run our oculus script

--- run_detection.sh
+++ run_detection.sh
@@ -4,9 +4,9 @@ protoc object_detection/protos/*.proto --python_out=.
 # From Oculus/
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
 
-# From Oculus/
-python object_detection/builders/model_builder_test.py
-
 # From Oculus/object_detection
 cd object_detection
-jupyter notebook object_detection_tutorial.ipynb
+python3 oculus.py
+
+# Come back to parent dir
+cd ..
