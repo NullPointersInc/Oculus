a301579b9d5db6e4ef35ec2bfac6e93c3807c6ca
Akash James <akash.james2096@gmail.com>
2018-03-20 13:16:03 +0530

    Oculus: task_manager: Rework on MQTT

--- task_manager.py
+++ task_manager.py
@@ -1,35 +1,111 @@
+"""Task mamanger script to unify all different features in Oculus."""
 import paho.mqtt.client as mqtt
 import time
 import os
+import argparse
+import sys
 
 
-def msg(client, userdata, message):
+def on_connect(client, userdata, flags, rc):
+    """on_connect is used to check connection to broker."""
+    if rc == 0:
+
+        print("Connected to broker: " + str(broker_address))
+        global Connected  # Use global variable
+        Connected = True  # Signal connection
+
+    else:
+        print("Connection failed to " + str(broker_address))
+
+
+def on_message(client, userdata, message):
+    """Perform operations when message is received."""
     op = str(message.payload.decode("utf-8"))
     if op == 'object':
         os.system("sh run_detection.sh -o")
-    elif op == 'money':
+    elif op == 'currency':
         time.sleep(3)
         os.system(
-            "sshpass -p 'qwerty12345' scp pi@192.168.0.5:~/oculus/image.jpg .")
-        os.system("mv image.jpg classify_note/")
+            "sshpass -p 'bella12345' \
+             scp pi@192.168.0.9:~/image/image.jpg ./classify_note/")
         os.system("sh run_detection.sh -n")
-    elif op == 'faces':
+    elif op == 'face':
         time.sleep(3)
         os.system(
-            "sshpass -p 'qwerty12345' scp pi@192.168.0.5:~/oculus/image.jpg .")
+            "sshpass -p 'bella12345' scp pi@192.168.0.9:~/image/image.jpg .")
         os.system("mv image.jpg classify_person/")
         os.system("sh run_detection.sh -p")
+    elif op == 'prediction':
+        time.sleep(3)
+        os.system(
+            "sshpass -p 'bella12345' \
+             scp pi@192.168.0.9:~/image/image.jpg ./im2text/data")
+        os.system("sh run_detection.sh -l")
+
+    elif op == 'ocr':
+        time.sleep(3)
+        os.system(
+            "sshpass -p 'bella12345' \
+             scp pi@192.168.0.9:~/image/image.jpg ./OCR")
+        os.system("sh OCR/ocr.py --image image.jpg")
+    else:
+        pass
 
 
-broker_addr = "192.168.0.5"
-client = mqtt.Client("tm")
-client.on_message = msg
+def get_args_values(args=None):
+    """Method to handle command line arguments."""
+    parser = argparse.ArgumentParser(description="Arguments supported..")
+    parser.add_argument('-H', '--host',
+                        help='Broker IP',
+                        default='localhost')
+    parser.add_argument('-p', '--port',
+                        help='port of the Broker',
+                        default='1883')
+    parser.add_argument('-u', '--user',
+                        help='user name',
+                        default='astr1x')
+    parser.add_argument('-P', '--password',
+                        help="password",
+                        default='astr1x2096')
 
-print ("Connecting to broker")
-client.connect(broker_addr)
-print ("Subscribing to topic oculus")
-while True:
-    client.loop_start()
-    client.subscribe("oculus")
-    time.sleep(1)
+    info = parser.parse_args(args)
+    return (info.host,
+            info.port,
+            info.user,
+            info.password)
+
+
+"""
+Get Broker address, port, username and password.
+The default value has already been provided.
+"""
+if __name__ == '__main__':
+    broker_address, port, user, password = get_args_values(sys.argv[1:])
+    port = int(port)
+
+
+Connected = False  # global variable for the state of the connection
+
+client = mqtt.Client("Oculus-Server")  # create new instance
+client.username_pw_set(user, password=password)  # set username and password
+client.on_connect = on_connect  # attach function to callback
+client.on_message = on_message  # attach function to callback
+
+
+print("Connecting to broker")
+client.connect(broker_address, port=port)  # connect to broker
+
+while Connected is not True:  # Wait for connection
+    time.sleep(0.1)
+
+print("Subscribing to topic Oculus")
+client.subscribe("Oculus")
+
+try:
+    while True:
+        time.sleep(1)
+except (KeyboardInterrupt, SystemExit):
+    print()
+    print("Exiting..")
+    client.disconnect()
     client.loop_stop()
