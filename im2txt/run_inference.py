f2db3ade13fd41b707877d7fb56cf4de2f8303e5
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-03-20 10:52:19 +0530

    Oculus: im2txt: Speak output using Google Speech

--- im2txt/run_inference.py
+++ im2txt/run_inference.py
@@ -28,13 +28,15 @@ from im2txt import configuration
 from im2txt import inference_wrapper
 from im2txt.inference_utils import caption_generator
 from im2txt.inference_utils import vocabulary
+from google_speech import Speech
 
 FLAGS = tf.flags.FLAGS
 
 tf.flags.DEFINE_string("checkpoint_path", "",
                        "Model checkpoint file or directory containing a "
                        "model checkpoint file.")
-tf.flags.DEFINE_string("vocab_file", "", "Text file containing the vocabulary.")
+tf.flags.DEFINE_string("vocab_file", "",
+                       "Text file containing the vocabulary.")
 tf.flags.DEFINE_string("input_files", "",
                        "File pattern or comma-separated list of file patterns "
                        "of image files.")
@@ -43,43 +45,57 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 
 def main(_):
-  # Build the inference graph.
-  g = tf.Graph()
-  with g.as_default():
-    model = inference_wrapper.InferenceWrapper()
-    restore_fn = model.build_graph_from_config(configuration.ModelConfig(),
-                                               FLAGS.checkpoint_path)
-  g.finalize()
+    # Build the inference graph.
+    g = tf.Graph()
+    with g.as_default():
+        model = inference_wrapper.InferenceWrapper()
+        restore_fn = model.build_graph_from_config(configuration.ModelConfig(),
+                                                   FLAGS.checkpoint_path)
+    g.finalize()
 
-  # Create the vocabulary.
-  vocab = vocabulary.Vocabulary(FLAGS.vocab_file)
+    # Create the vocabulary.
+    vocab = vocabulary.Vocabulary(FLAGS.vocab_file)
 
-  filenames = []
-  for file_pattern in FLAGS.input_files.split(","):
-    filenames.extend(tf.gfile.Glob(file_pattern))
-  tf.logging.info("Running caption generation on %d files matching %s",
-                  len(filenames), FLAGS.input_files)
+    filenames = []
+    for file_pattern in FLAGS.input_files.split(","):
+        filenames.extend(tf.gfile.Glob(file_pattern))
+    tf.logging.info("Running caption generation on %d files matching %s",
+                    len(filenames), FLAGS.input_files)
 
-  with tf.Session(graph=g) as sess:
-    # Load the model from checkpoint.
-    restore_fn(sess)
+    with tf.Session(graph=g) as sess:
+        # Load the model from checkpoint.
+        restore_fn(sess)
 
-    # Prepare the caption generator. Here we are implicitly using the default
-    # beam search parameters. See caption_generator.py for a description of the
-    # available beam search parameters.
-    generator = caption_generator.CaptionGenerator(model, vocab)
+        # Prepare the caption generator. Here we are implicitly using
+        # the default
+        # beam search parameters. See caption_generator.py for a description
+        # of the
+        # available beam search parameters.
+        generator = caption_generator.CaptionGenerator(model, vocab)
 
+    speak_string = ["It looks like the situation is one of the following: "]
     for filename in filenames:
-      with tf.gfile.GFile(filename, "rb") as f:
-        image = f.read()
-      captions = generator.beam_search(sess, image)
-      print("Captions for image %s:" % os.path.basename(filename))
-      for i, caption in enumerate(captions):
-        # Ignore begin and end words.
-        sentence = [vocab.id_to_word(w) for w in caption.sentence[1:-1]]
-        sentence = " ".join(sentence)
-        print("  %d) %s (p=%f)" % (i, sentence, math.exp(caption.logprob)))
+        with tf.gfile.GFile(filename, "rb") as f:
+            image = f.read()
+        captions = generator.beam_search(sess, image)
+        print("Captions for image %s:" % os.path.basename(filename))
+        for i, caption in enumerate(captions):
+            # Ignore begin and end words.
+            sentence = [vocab.id_to_word(w) for w in caption.sentence[1:-1]]
+            sentence = " ".join(sentence)
+            if i == 0:
+                speak_string.append("Either, " + sentence)
+            elif i == 1:
+                speak_string.append("Or, " + sentence)
+            elif i == 2:
+                speak_string.append("Or maybe, " + sentence)
+            print("  %d) %s (p=%f)" % (i, sentence, math.exp(caption.logprob)))
+
+    for speak_line in speak_string:
+        speech = Speech(speak_line, lang="en")
+        sox_effects = ("speed", "1")
+        speech.play(sox_effects)
 
 
 if __name__ == "__main__":
-  tf.app.run()
+    tf.app.run()
