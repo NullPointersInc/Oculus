Oculus: Frame rotation fix

classify_person/classify.py
@@ -1,13 +1,22 @@
 import math
 import os
 import sys
+import imutils
+import cv2
 
 import tensorflow as tf
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# change this as you see fit
 image_path = sys.argv[1]
+
+image = cv2.imread(image_path)
+image = imutils.rotate(image, 270)
+cv2.imwrite('image.jpg', image)
+# change this as you see fit
+
+image_path = "image.jpg"
+
 
 # Read in the image_data
 image_data = tf.gfile.FastGFile(image_path, 'rb').read()